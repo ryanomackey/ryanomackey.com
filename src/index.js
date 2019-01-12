662862ec5220c48f2a55d73ff8187f138a8802de
Ryan O. Mackey <ryan.o.mackey@gmail.com>
2019-01-12 16:23:28 -0600

    fix: add intersection-observer polyfill

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// Polyfills
+import 'intersection-observer';
+
 import App from './App';
 
 const root = document.getElementById('root');
